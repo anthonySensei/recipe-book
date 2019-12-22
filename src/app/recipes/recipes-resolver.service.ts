8eccad5d9dc9f432b8224bc383b697a6bb4441d2
anthonySensei <toxa13.00@ukr.net>
2019-12-22 16:53:32 +0200

    v1.9

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -1,9 +1,11 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
+
 import {Recipe} from './recipe.model';
+
 import {DataStorageService} from '../shared/data-storage.service';
-import {Observable} from 'rxjs';
 import {RecipeService} from './recipe.service';
+
 
 @Injectable({providedIn: 'root'})
 export class RecipesResolverService implements Resolve<Recipe[]>{
@@ -13,7 +15,7 @@ export class RecipesResolverService implements Resolve<Recipe[]>{
     const recipes = this.recipeService.getRecipes();
 
     if(recipes.length === 0){
-    return this.dataStorageService.fetchRecipes();
+      return this.dataStorageService.fetchRecipes();
     }else{
       return recipes;
     }
